ea8c330ece826d4287f2ce9488e5c4c65a89f889
pulkit-singhall <singhal.pulkit667@gmail.com>
2024-05-05 00:18:50 +0530

    initial commit

---
+++ src/app/api/query/send/route.ts
@@ -0,0 +1,11 @@
+import ApiResponse from "@/types/response";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        
+    } catch (error) {
+        const { name, email, query } = await req.json()
+        
+    }
+}
---
+++ src/types/response.ts
@@ -0,0 +1,20 @@
+export default class ApiResponse{
+    success: boolean
+    message: string
+    status: number
+    data: any
+    error: any
+    constructor(
+        status: number,
+        success: boolean,
+        message: string,
+        data: any,
+        error: any
+    ) {
+        this.success = success
+        this.status = status
+        this.data = data
+        this.message = message
+        this.error = error
+    }
+}
